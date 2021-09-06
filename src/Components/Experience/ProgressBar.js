1e50941c58d4a705588ca297af60a07b2425ac59
marwa <marwajawad19@gmail.com>
2021-09-06 12:34:20 +0300

    crating propgress bar widget

---
+++ src/Components/Experience/ProgressBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+const Index = () => {
+  return (
+    <div>
+      <div>Web development - 11 years</div>
+      <div>Photography - 3 years </div>
+      <div>Marketing - 2 years</div>
+      <div>Design - 8 years</div>
+    </div>
+  );
+};
+
+export default Index;
