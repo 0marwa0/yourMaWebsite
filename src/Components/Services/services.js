1e84077c3b5886bdc858ccbba514e2fc24dd4475
marwa <marwajawad19@gmail.com>
2021-09-06 12:35:24 +0300

    adding services data

---
+++ src/Components/Services/services.js
@@ -0,0 +1,21 @@
+const services = [
+  {
+    image: "",
+    name: "web Development",
+    description:
+      "Maecenas sit amet commodo tellus. Phasellus fermentum pretium eros, ut faucibus velit auctor eget. Vestibulum semper pharetra. Curabitur porta dapibus.",
+  },
+  {
+    image: "",
+    name: "Project Management",
+    description:
+      "Dolor curabitur et mattis ante. Maecenas sit amet – commodo tellus. Phasellus fermentum pretium eros, ut faucibus velit auctor dolor eget.",
+  },
+  {
+    image: "",
+    name: "Online Marketing",
+    description:
+      "Curabitur et mattis ante. Maecenas sit amet – commodo tellus. Phasellus fermentum pretium eros, ut faucibus velit auctor eget. Vestibulum lorem ipsum semper pharetra.",
+  },
+];
+export default services;
