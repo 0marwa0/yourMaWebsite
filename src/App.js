1820da6384d51099ca32cdf278e4ece6c7ce3437
marwa <marwajawad19@gmail.com>
2021-09-06 12:53:26 +0300

    adding style folder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import Experinence from "./Components/Experience";
+import Services from "./Components/Services";
 function App() {
-  return <div />;
+  return (
+    <div>
+      <Experinence />
+      <Services />
+    </div>
+  );
 }
 
 export default App;
