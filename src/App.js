628767eb5284d9e0dfa1149e600e69dd2c671865
marwa <marwajawad19@gmail.com>
2021-09-07 05:35:09 +0300

    adding services, contact, experience and footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import Contact from "./Components/contact";
 import Experinence from "./Components/Experience";
 import Services from "./Components/Services";
+import "./Style/app.css";
+import Footer from "./Components/footer";
 function App() {
   return (
     <div>
       <Experinence />
       <Services />
+      <Contact />
+      <Footer />
     </div>
   );
 }
