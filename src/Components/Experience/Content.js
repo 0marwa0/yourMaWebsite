bbc6a261375a500945c2508ad6950cc717e47df4
marwa <marwajawad19@gmail.com>
2021-09-07 05:31:02 +0300

    adding experience text in experience section

--- src/Components/Experience/Content.js
+++ src/Components/Experience/Content.js
@@ -1,6 +1,22 @@
 import React from "react";
+import Progress from "./Progress";
 const Index = () => {
-  return <div></div>;
+  return (
+    <div className="flex flex-col" style={{ width: "500px" }}>
+      <p className="font-extrabold text-2xl">Our experience</p>
+
+      <div className="w-2" />
+      <h2 className="font-bold text-xl mt-2 mb-2">
+        Suscipit felis congue ut. Vivamus ut dolor ametis.
+      </h2>
+      <div>
+        Curabitur et mattis ante. Maecenas sit amet commodo tellus. Phasellus
+        fermentum pretium eros, ut faucibus velit auctor eget. Vestibulum semper
+        sed magna pretium congue. Fusce eget arcu eget nisi mattis lacinia sit
+        amet at sem. Quisque porttitor bibendum leo ornare pharetra
+      </div>
+    </div>
+  );
 };
 
 export default Index;
