deb88a560e17ccc88f23e1bcc65d5f29cc76d781
marwa <marwajawad19@gmail.com>
2021-09-07 05:27:25 +0300

    create fake data for post section

---
+++ src/Components/footer/Posts.js
@@ -0,0 +1,22 @@
+import post1 from "../../assets/images/post1.jpg";
+import post2 from "../../assets/images/post2.jpg";
+import post3 from "../../assets/images/post3.jpg";
+
+const Posts = [
+  {
+    image: post1,
+    date: "November 13,2019",
+    name: "Fusce aliquam – nunc ac suscipit",
+  },
+  {
+    image: post2,
+    date: "November 13,2019",
+    name: "Aliquam erat volutpat",
+  },
+  {
+    image: post3,
+    date: "November 13,2019",
+    name: "Quisque semper malesuada ipsum",
+  },
+];
+export default Posts;
