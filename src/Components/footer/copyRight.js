f78cbbb0aa27d331b9f5826d6200d8e39aeaacc5
marwa <marwajawad19@gmail.com>
2021-09-07 05:26:43 +0300

    copy right section to the footer

---
+++ src/Components/footer/copyRight.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div className="flex items-center gap-5 pl-20 pr-20 pb-10 bg-black space-x-80 ">
+      <div className="text-white"> © your MA - 2021. All rights reserved. </div>
+    </div>
+  );
+};
+
+export default Index;
