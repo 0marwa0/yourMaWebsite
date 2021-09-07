9d74fc0a0bda6465b066ed783095294c154fa82e
marwa <marwajawad19@gmail.com>
2021-09-07 05:42:01 +0300

    create services cards component

--- src/Components/Services/services.js
+++ src/Components/Services/services.js
@@ -1,18 +1,21 @@
+import web from "../../assets/images/web.jpg";
+import project from "../../assets/images/project.jpg";
+import marketing from "../../assets/images/marketing.jpg";
 const services = [
   {
-    image: "",
+    image: web,
     name: "web Development",
     description:
       "Maecenas sit amet commodo tellus. Phasellus fermentum pretium eros, ut faucibus velit auctor eget. Vestibulum semper pharetra. Curabitur porta dapibus.",
   },
   {
-    image: "",
+    image: project,
     name: "Project Management",
     description:
       "Dolor curabitur et mattis ante. Maecenas sit amet – commodo tellus. Phasellus fermentum pretium eros, ut faucibus velit auctor dolor eget.",
   },
   {
-    image: "",
+    image: marketing,
     name: "Online Marketing",
     description:
       "Curabitur et mattis ante. Maecenas sit amet – commodo tellus. Phasellus fermentum pretium eros, ut faucibus velit auctor eget. Vestibulum lorem ipsum semper pharetra.",
